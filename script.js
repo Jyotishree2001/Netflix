36dd077ca49c12758216527d7c4680c1d0e3a9f4
MilindGupta-Creator <milindgupta578@gmail.com>
2020-10-02 09:45:13 +0530

    clock added

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+let digitalclock = () => {
+
+    let date = new Date();
+
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    
+
+    let amorpm = hours >= 12 ? 'pm' : 'am';
+
+    document.getElementById("hours").innerHTML=hours;
+    document.getElementById("minutes").innerHTML=minutes;
+    document.getElementById("seconds").innerHTML=seconds;
+    document.getElementById("amorpm").innerHTML=amorpm;
+    
+
+    setTimeout(digitalclock,100);
+
+}
+
+digitalclock();
